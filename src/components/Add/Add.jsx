25b9f55c160f799cfadd8010aa629ac07cbb62d4
OlegRosinskyi <olegrosinskyi@gmail.com>
2023-02-24 21:30:28 +0200

    update
    update

---
+++ src/components/Add/Add.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class Add extends Component{
+     
+   
+    
+      render (){
+    return (
+        <> 
+            <button type="button" >Load more</button>             
+        </> 
+     )}
+}
